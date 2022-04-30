8b5ce9cd385ab5fb08d0ddba972ca9b6b2a4bf83
azusa <azusa.222@gmail.com>
2022-04-30 19:27:11 +0900

    トップページもスプレッドシートから読み込んでリストにするように修正。

---
+++ csv_data_get.js
@@ -0,0 +1,85 @@
+const COL_NO_TIME_STAMP = 0;
+const COL_NO_MAIL = 1;
+const COL_NO_NAME = 2;
+const COL_NO_TOU = 3;
+const COL_NO_IMAGE = 4;
+const COL_NO_Q_1 = 5;
+const COL_NO_Q_2 = 7;
+const COL_NO_Q_3 = 6;
+const COL_NO_Q_4 = 8;
+const COL_NO_Q_ANS = 9;
+const COL_NO_FREE = 10;
+const COL_NO_TYPE = 11;
+
+const TYPE_KUCHO = '"中野区長選挙"';
+const TYPE_KUGI = '"中野区議会議員補欠選挙"';
+
+function csv_data_get(dataPath) {
+    // CSVファイルを文字列として取得
+    let srt = new XMLHttpRequest();
+    srt.open("GET", dataPath, false);
+    try {
+        srt.send(null);
+    } catch (err) {
+        console.log(err);
+    }
+
+    // 配列を用意
+    let csletr = [];
+
+    // 改行ごとに配列化
+    let lines = srt.responseText.split("\n");
+
+    // 1行ごとに処理
+    for (let i = 0; i < lines.length; ++i) {
+        let cells = lines[i].split(",");
+        if (cells.length != 1) {
+            csletr.push(cells);
+        }
+    }
+    return csletr;
+
+}
+
+// CSVを読みだしたリストから、指定した大部のデータのみ取得する
+// type: TYPE_KUCHO, TYPE_KUGI
+function get_type(csv_list, type){
+     // 配列を用意
+     let csletr = [];
+     for (var i = 0; i < csv_list.length; i++) { 
+         let data = csv_list[i];
+         if(data[COL_NO_TYPE] == type){
+             csletr.push(data);
+         }
+     }
+     return csletr;
+    
+}
+
+function draw_top(kouho, type){
+    let divListList = [];
+
+    for (var i = 0; i < kouho.length; i++) { 
+        var divList = document.createElement('div'); 
+        divList.setAttribute('class', "col-md-6"); 
+        divList.textContent = kouho[i][COL_NO_NAME];
+        
+        let divItem = document.createElement('div');
+        divItem.setAttribute('class', "col-3");
+        
+        let image = document.createElement('img');
+        if(type == TYPE_KUCHO){
+            image.src = 'pic/kucho' + i + ".jpg";
+        } else {
+            image.src = 'pic/kugi' + i + ".jpg";
+        }
+        image.setAttribute('class', 'img-fluid');
+        divItem.appendChild(image); 
+
+        divList.appendChild(divItem);
+    
+        divListList.push(divList);
+    }
+    return divListList;
+}
+
